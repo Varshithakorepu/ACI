9de3d2aa72475146686a18d7bf7c8648d290e46c
Varshithakorepu <varshithakorepu1105@gmail.com>
2025-11-30 19:22:26 +0530

    Added files

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require('http');
+	const PORT = process.env.PORT || 8081;
+	const server = http.createServer((req, res) => {
+	  res.writeHead(200, {'Content-Type':'text/plain'});
+	  res.end('Hello from project1 Docker container!\n');
+	});
+	server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
